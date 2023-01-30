2524a93d506e473a8269950de21d9d8d40fc72e1
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-01-30 17:41:43 +0600

    toFixed method

--- 16/types.js
+++ 16/types.js
@@ -1,2 +1,17 @@
 var price = 45;
 console.log(typeof price);
+
+
+var asthetic;
+console.log(typeof asthetic);
+
+
+let num1 = 0.3;
+let num2 = 0.5;
+
+let sum = num1 + num2;
+
+sum = sum.toFixed(3);
+
+console.log(sum);
+
