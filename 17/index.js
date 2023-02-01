e053fefe176694721b047ecd91e9ea05ff829c8b
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-01 22:59:54 +0600

    set element value

--- 17/index.js
+++ 17/index.js
@@ -1,4 +1,11 @@
 const numbers = [34, 45, 65, 23, 81.5, 99, 14];
 
 // Get element by index
-console.log(numbers[4]);
+console.log(numbers);
+
+// set element value by index
+numbers[2] = 77;
+numbers[5] = 39;
+
+console.log(numbers);
+
