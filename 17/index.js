7066c273d614cd0d43301b8c4945ada35a54484a
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-01 23:16:56 +0600

    push-pop

--- 17/index.js
+++ 17/index.js
@@ -1,4 +1,4 @@
-const numbers = [34, 45, 65, 23, 81.5, 99, 14];
+const numbers = [34, 45, 65, 23, 81, 99, 14];
 
 // Get element by index
 console.log(numbers);
---
+++ 17/push-pop.js
@@ -0,0 +1,8 @@
+let numbers = [23, 43, 21, 64, 47, 89];
+console.log(numbers);
+
+// numbers.push(77);
+// console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
