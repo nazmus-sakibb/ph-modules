b659e71d6f5b17ccba2ef8fd9bb44c360915d810
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-05 10:56:48 +0600

    even number by funciton

---
+++ 20/evenOdd.js
@@ -0,0 +1,9 @@
+function isEven(number){
+    if(number % 2 === 0){
+        console.log('number is even', number);
+    } else {
+        console.log('number is odd', number);
+    }
+}
+
+isEven(303);
