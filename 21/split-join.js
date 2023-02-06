e885d3c57a01b5563404992f0430866954c8425d
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-06 23:05:18 +0600

    split method in js

---
+++ 21/split-join.js
@@ -0,0 +1,3 @@
+const lyrics = 'Tmi bondhu kala pakhi ami jeno ki. Bosonto kale tomai bolte pari ni.Sada sada kala kala Rong jomeche sada kala Hoichi ami mon pagla Bosonto kaale';
+const parts = lyrics.split();
+console.log(parts);
