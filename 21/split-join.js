84a9a139d7c35cbc260d6c4e0b9f9919f02b2b58
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-06 23:09:12 +0600

    split characters

--- 21/split-join.js
+++ 21/split-join.js
@@ -1,3 +1,5 @@
-const lyrics = 'Tmi bondhu kala pakhi ami jeno ki. Bosonto kale tomai bolte pari ni.Sada sada kala kala Rong jomeche sada kala Hoichi ami mon pagla Bosonto kaale';
-const parts = lyrics.split();
-console.log(parts);
+const lyrics = 'Tmi bondhu kala pakhi ami jeno ki. Bosonto kale tomai bolte pari ni. Sada sada kala kala Rong jomeche sada kala Hoichi ami mon pagla Bosonto kaale.';
+const parts = lyrics.split(' ');
+const sentences = lyrics.split('.');
+const chars = lyrics.split('');
+console.log(chars);
