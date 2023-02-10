65143f34dd0fca859294ff010b700372c5d708bd
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-02-10 10:47:27 +0600

    event handler on button

--- 25-dom-event/1/js/event.js
+++ 25-dom-event/1/js/event.js
@@ -2,4 +2,14 @@ console.log("this is seperate js file");
 
 function makeRed(){
     document.body.style.backgroundColor = "red";
+} 
+
+
+const buttonBlue = document.getElementById("blue");
+buttonBlue .onclick = makeBlue;
+
+
+
+function makeBlue (){
+    document.body.style.backgroundColor = 'blue';
 }
