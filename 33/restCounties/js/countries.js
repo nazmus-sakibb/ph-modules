98858f6cfb74956d282895a682c199c93c153f64
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-04 13:26:06 +0600

    fetch rest countries api

--- 33/restCounties/js/countries.js
+++ 33/restCounties/js/countries.js
@@ -1,3 +1,12 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
 }
+
+const displayCountries = countries => {
+
+}
+
+
+loadCountries();
