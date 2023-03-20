8e7a3292b23e96a14f6e2ff04cf9ce848a852656
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-20 15:06:29 +0600

    fetch rest countries api

--- 46/react-rest-countries/src/App.js
+++ 46/react-rest-countries/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries />
     </div>
   );
+}
+
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+  return (
+    <div>
+      <h2>Rest countries</h2>
+      <h4>Available countries: {countries.length}</h4>
+    </div>
+  )
 }
 
 export default App;
