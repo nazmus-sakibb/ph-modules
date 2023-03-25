f90598f0db1dc249f7d3922a584a3adc0dc42c47
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-25 14:05:55 +0600

    tailwind css initialize

--- 51/react-with-bootstrap/src/App.js
+++ 51/react-with-bootstrap/src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Spinner } from 'react-bootstrap';
-import CardGrid from './components/CardGrid/CardGrid';
 
 function App() {
   return (
     <div className="App">
-      <Button variant='danger'>Button</Button>
-      <Spinner animation="border" variant="success" />
-      <CardGrid/>
+      <h1>This is header</h1>
+      <p>paragraph</p>
     </div>
   );
 }
