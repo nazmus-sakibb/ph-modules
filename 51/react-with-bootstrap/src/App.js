80e0adeefeb1af2fa91e1b4336f790a392ec8f3a
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-25 12:50:12 +0600

    card group componet of react-bootstrap

--- 51/react-with-bootstrap/src/App.js
+++ 51/react-with-bootstrap/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Spinner } from 'react-bootstrap';
+import CardGrid from './components/CardGrid/CardGrid';
 
 function App() {
   return (
     <div className="App">
-      <Button
+      <Button variant='danger'>Button</Button>
+      <Spinner animation="border" variant="success" />
+      <CardGrid/>
     </div>
   );
 }
