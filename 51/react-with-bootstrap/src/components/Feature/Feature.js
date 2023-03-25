2e8201ec168309e66a7c6e36b8f2b4da9864c7c1
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-26 00:59:36 +0600

    feature card layout

--- 51/react-with-bootstrap/src/components/Feature/Feature.js
+++ 51/react-with-bootstrap/src/components/Feature/Feature.js
@@ -1,11 +1,11 @@
 import { CheckCircleIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
-const Feature = ({featuer}) => {
+const Feature = ({ feature }) => {
     return (
         <div className='flex items-center'>
             <CheckCircleIcon className='h-5 w-5 text-green-700' />
-            <p className='ml-2'>Awesome Feature</p>
+            <p className='ml-2'>{feature}</p>
         </div>
     );
 };
--- 51/react-with-bootstrap/src/components/PriceOption/PriceOption.js
+++ 51/react-with-bootstrap/src/components/PriceOption/PriceOption.js
@@ -3,6 +3,7 @@ import Feature from '../Feature/Feature';
 
 const PriceOption = ({ option }) => {
     const { features } = option;
+    console.log(features);
     return (
         <div className='bg-indigo-300 m-3 rounded-md p-3'>
             <div>
@@ -12,8 +13,9 @@ const PriceOption = ({ option }) => {
                 <p className='text-3xl my-4'>{option.name}</p>
             </div>
             {
-                features.map(feature => <Feature feature={feature}/>)
+                features.map((feature, idx) => <Feature feature={feature} key={idx}/>)
             }
+            <button className='bg-purple-500 w-full rounded-md py-2 font-bold mt-4'>Buy Now</button>
         </div>
     );
 };
