69075a3ece40753a11ef56d427f58e79a82487c7
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-26 16:01:49 +0600

    load users

--- 52/react-router/src/components/Friend/Friend.js
+++ 52/react-router/src/components/Friend/Friend.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Friend.css';
 
-const Friend = ({friend}) => {
-    const {name} = friend;
+const Friend = ({ friend }) => {
+    const { id, name, email, username } = friend;
     return (
-        <div>
-            <p>Name: {name}</p>
+        <div className='friend'>
+            <h4>Name: {name}</h4>
+            <p>Email: {email}</p>
+            <p><small>Username: <Link to={`/friend/${id}`}>{username}</Link></small></p>
         </div>
     );
 };
