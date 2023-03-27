8ba3a3bb2ff0974367e6326ae6a8fe1503eda3d4
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-03-27 19:57:37 +0600

    dynamic button

--- 52/react-router/src/components/Post/Post.js
+++ 52/react-router/src/components/Post/Post.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Post.css';
 
 const Post = ({ post }) => {
     const { id, title, body } = post;
+
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate(`/post/${id}`)
+    }
     return (
         <div className='post'>
             <p>{title}</p>
             <p><small>{body}</small></p>
+            <Link to={`/post/${id}`}>visit: {id}</Link>
             <Link to={`/post/${id}`}>
                 <button>Show Details</button>
             </Link>
+            <button onClick={handleNavigate}>Show details2</button>
         </div>
     );
 };
