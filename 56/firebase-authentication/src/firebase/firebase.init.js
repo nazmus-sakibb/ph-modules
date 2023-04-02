027901295c57c5bd647ee57702fd4909ed389a6a
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-02 21:16:47 +0600

    firebase setup

---
+++ 56/firebase-authentication/src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjQz8D8a5a16dOJmytIkT0eIRNK1Jc8xs",
+  authDomain: "fire-auth-7.firebaseapp.com",
+  projectId: "fire-auth-7",
+  storageBucket: "fire-auth-7.appspot.com",
+  messagingSenderId: "600940955591",
+  appId: "1:600940955591:web:05c31ef7dc3649fc06bbe2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
