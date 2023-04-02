adbbc82e602acb9e97d598370685afbab839478a
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-02 13:02:58 +0600

    make api for phones

--- 55/phone-hunter/index.js
+++ 55/phone-hunter/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const phones = require('./phones.json');
 const port = 5000;
 
-app.get('/', (req, res) =>{
-    res.send('Hello world');
+app.get('/', (req, res) => {
+    res.send('Look mama again but not again! Hello world');
 });
+
+app.get('/phones', (req, res) => {
+    res.send(phones);
+})
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('looking for data of id:', id);
+    const phone = phones.find(ph => ph.id === id) || {};
+    res.send(phone);
+})
 
 app.listen(port, () => {
     console.log(`Phone server is runnnig on port: ${port}`);
