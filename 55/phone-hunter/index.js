a713eb819cb22243df1dc900298cb0260af06ed8
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-02 14:40:35 +0600

    cors middleware

--- 55/phone-hunter/index.js
+++ 55/phone-hunter/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const phones = require('./phones.json');
 const port = 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Look mama again but not again! Hello world');
