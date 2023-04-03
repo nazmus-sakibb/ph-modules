63f7b46c76c331d8145c372741d089e29bac2fca
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-03 19:37:08 +0600

    display logged in user data

--- 56/firebase-authentication/src/App.js
+++ 56/firebase-authentication/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch(error => {
@@ -21,6 +24,11 @@ function App() {
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <div>
+        <h3>Username: {user.displayName}</h3>
+        <p>Email: {user.email}</p>
+        <img src={user.photoURL} alt="" />
+      </div>
     </div>
   );
 }
