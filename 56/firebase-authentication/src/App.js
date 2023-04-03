ec0e7975c777fe1e9161ee0059a56e9b59fd5a81
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-03 17:27:46 +0600

    intigrate google sign in button

--- 56/firebase-authentication/src/App.js
+++ 56/firebase-authentication/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+import app from './firebase/firebase.init';
 
+const auth = getAuth(app);
 function App() {
+  const provider = GoogleAuthProvider();
+
+  const handleGoogleSingIn = () => {
+    console.log("google coming soon!");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSingIn}>Google Sign In</button>
     </div>
   );
 }
