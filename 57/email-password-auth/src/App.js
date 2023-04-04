29efde99d89cb1b134f8ac284bb8491c38371ec1
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-05 01:10:57 +0600

    add event handler for getting input value

--- 57/email-password-auth/src/App.js
+++ 57/email-password-auth/src/App.js
@@ -4,13 +4,21 @@ import app from './firebase/firebase.init';
 
 const auth = getAuth(app);
 
+const handleRegister = (e) => {
+  e.preventDefault();
+  const email = e.target.email.value;
+  const password = e.target.password.value;
+  console.log(email, password);
+  
+}
+
 function App() {
   return (
     <div className="App">
-      <form>
-        <input type="email" placeholder="Your Email..."/>
+      <form onSubmit={handleRegister}>
+        <input type="email" name="email" placeholder="Your Email..."/>
         <br />
-        <input type="password" name="" id="" placeholder="Your Password"/>
+        <input type="password" name="password" id="" placeholder="Your Password"/>
         <br />
         <button type="submit">Register</button>
       </form>
