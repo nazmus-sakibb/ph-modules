d2c96d9e8da9aebe6f97a03194cde2f471702bd7
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-04-06 16:37:08 +0600

    login and register form create

--- 58/auth-router-context/src/components/Login.js
+++ 58/auth-router-context/src/components/Login.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Login = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target; 
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password); 
+    }
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -9,18 +16,18 @@ const Login = () => {
                         <h1 className="text-5xl font-bold">Please Login now!</h1>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
+                        <form onSubmit={handleSubmit} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input name='email' type="email" placeholder="email" className="input input-bordered" required/>
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <input name='password' type="password" placeholder="password" className="input input-bordered" required/>
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
@@ -28,7 +35,7 @@ const Login = () => {
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div> 
